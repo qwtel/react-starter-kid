d6a12cd4808dd7bafe1a5996db16fc3aa0cc71c9
Florian Klampfer <f.klampfer@gmail.com>
2016-05-09 10:48:30 +0200

    Fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var React = require('react');
-var {render} = require('react-dom');
+var ReactDOM = require('react-dom');
 
 var App = require('./components/App');
 
-render(React.createElement(App), document.getElementById('app'));
+ReactDOM.render(React.createElement(App), document.getElementById('app'));
