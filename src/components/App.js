9ba0ca4403264402151afecdc38de08392dd551b
Florian Klampfer <f.klampfer@gmail.com>
2016-05-09 11:13:59 +0200

    Make lint compatible

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,13 @@ import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, World!</h1>
-        <AddTodo />
-        <VisibleTodoList />
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <h1>Hello, World!</h1>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+);
 
 export default App;
